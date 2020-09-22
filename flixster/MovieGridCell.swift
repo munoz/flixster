d425c3c745d5b379b0bf80222e32605d287ec4b0
Pedro Muñoz <pmunoz@csumb.edu>
2020-09-21 23:13:39 -0700

    Part 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+ //
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by studentProfile on 9/21/20.
+//  Copyright © 2020 munoz. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+ }
